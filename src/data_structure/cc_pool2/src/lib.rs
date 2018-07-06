4f1ea597130874eeae6fa0e1d4b403bc8e6efd7b
Jonathan Simms <jsimms@twitter.com>
2018-07-05 21:22:13 -0400

    pool test

--- src/data_structure/cc_pool2/src/lib.rs
+++ src/data_structure/cc_pool2/src/lib.rs
@@ -1,17 +1,30 @@
 extern crate bytes;
+extern crate failure;
+
+use pool::{FnWrapper, ObjectInitFnPtr, Pool};
+use std::rc::Rc;
 
 mod pool;
 
-type ObjectInitFn = unsafe extern "c" fn(*mut u8);
 
 #[repr(C)]
 pub struct PoolHandle {
     inner: pool::Pool,
-    init_fn: ObjectInitFn,
+    c_callback: Rc<FnWrapper>,
 }
 
 #[no_mangle]
-pub extern "C" fn pool2_create_handle(szof: usize, nmax: usize, initf: ObjectInitFn) -> *const PoolHandle {
-    pool::Pool::new(szof, nmax)
+pub extern "C" fn pool2_create_handle(
+    szof: usize, nmax: usize, initf: ObjectInitFnPtr
+) -> *const PoolHandle
+{
+    let fw = Rc::new(FnWrapper::new(initf));
+
+    let ph = PoolHandle {
+        c_callback: fw.clone(),
+        inner: Pool::new(szof, nmax, fw.clone()),
+    };
+
+    Box::into_raw(Box::new(ph))
 }
 
--- src/data_structure/cc_pool2/src/pool/mod.rs
+++ src/data_structure/cc_pool2/src/pool/mod.rs
@@ -1,5 +1,44 @@
-use std::collections::VecDeque;
+use failure;
 use std;
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::result;
+
+pub(crate) type Result<T> = result::Result<T, failure::Error>;
+pub(crate) type ObjectInitFnPtr = unsafe extern "C" fn(*mut u8);
+
+pub trait BufCallback {
+    fn callback(&self, buf: &mut [u8]);
+}
+
+struct ClosureWrapper(Box<Fn(&mut [u8])>);
+
+impl ClosureWrapper {
+    fn new<T: Fn(&mut [u8]) + 'static>(f: T) -> Rc<BufCallback> {
+        Rc::new(ClosureWrapper(Box::new(f)))
+    }
+}
+
+impl BufCallback for ClosureWrapper {
+    fn callback(&self, buf: &mut [u8]) {
+        (self.0)(&mut buf[..])
+    }
+}
+
+
+#[repr(C)]
+pub struct FnWrapper(ObjectInitFnPtr);
+
+impl FnWrapper {
+    pub fn new(f: ObjectInitFnPtr) -> Self { FnWrapper(f) }
+}
+
+impl BufCallback for FnWrapper {
+    fn callback(&self, buf: &mut [u8]) {
+        unsafe { (self.0)(buf.as_mut_ptr()) }
+    }
+}
+
 
 // we can either have a VecDeque of Box<[u8]>, which is like an array
 // of (bstring *), or we could contiguously allocate a Vec<u8> and carve
@@ -11,16 +50,14 @@ pub struct Pool {
     obj_size: usize,
     nused: usize,
     nmax: usize,
-    initfn: Box<FnMut(&mut Box<[u8]>)>,
+    initfn: Rc<BufCallback>,
 }
 
 // |<----------- nmax ---------->|
 // | nused | freeq     |  slack  |
 
 impl Pool {
-    pub fn new<F>(obj_size: usize, nmax: usize, initfn: F) -> Pool
-    where F: FnMut(&mut Box<[u8]>) + 'static
-    {
+    pub fn new(obj_size: usize, nmax: usize, initfn: Rc<BufCallback>) -> Pool {
         Pool{
             freeq: VecDeque::with_capacity(nmax),
             nused: 0,
@@ -30,20 +67,21 @@ impl Pool {
                     _ => nmax,
                 },
             obj_size,
-            initfn: Box::new(initfn),
+            initfn,
         }
     }
 
     pub fn prealloc(&mut self, size: usize) {
         // this doesn't check nmax?
         while self.freeq.len() < size {
-            self.freeq.push_back(self.allocate_one());
+            let v = self.allocate_one();
+            self.freeq.push_back(v);
         }
     }
 
     fn allocate_one(&mut self) -> Box<[u8]> {
         let mut bs = vec![0u8; self.obj_size].into_boxed_slice();
-        self.initfn(&mut bs);
+        self.initfn.callback(&mut bs[..]);
         bs
     }
 
@@ -68,5 +106,48 @@ impl Pool {
     pub fn unborrow(&mut self, item: Box<[u8]>) {
         self.freeq.push_back(item);
         self.nused -= 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_prealloc_and_alloc_and_new() {
+        let objsz = 5;
+        let nmax = 10;
+        let mut p = Pool::new(objsz, nmax, ClosureWrapper::new(|mut buf| buf[0] = 1u8 ));
+
+        assert_eq!(p.nused, 0);
+        assert_eq!(p.nmax, 10);
+        assert_eq!(p.freeq.len(), 0);
+        assert!(p.freeq.capacity() >= 10);
+
+        p.prealloc(3);
+        assert_eq!(p.freeq.len(), 3);
+
+        // make sure the callback was called
+        for b in p.freeq.iter() {
+            assert_eq!(b.len(), objsz);
+            assert_eq!(b[0], 1u8)
+        }
+    }
+
+    #[test]
+    fn test_borrow_and_unborrow() {
+        let objsz = 5;
+        let nmax = 2;
+        let mut p = Pool::new(objsz, nmax, ClosureWrapper::new(|buf| buf[0] = 1u8 ));
+
+        p.prealloc(1);
+
+        let a = p.borrow().unwrap();
+        let b = p.borrow().unwrap();    // this should allocate because we're still under nmax
+        assert!(p.borrow().is_none());  // sorry we're full
+
+        p.unborrow(a);
+        p.unborrow(b);
+        assert_eq!(p.freeq.len(), 2);
     }
 }
